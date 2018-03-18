067d1bb6a6a03dfaf76ad9434ba742609adf9fbd
Liam McDermott <liam@woolwichweb.works>
2018-03-18 00:59:05 -0400

    Fixed type of element map will be loaded into to better work with gettElementById.

--- src/Map.purs
+++ src/Map.purs
@@ -2,19 +2,19 @@ module GMaps.Map (Map (), gMap, panTo) where
 
 import Prelude (Unit)
 import Control.Monad.Eff (Eff)
-import DOM.HTML.Types (HTMLElement)
+import DOM.Node.Types (Element)
 import GMaps.LatLng (LatLng)
 import GMaps.MapOptions (MapOptions, runMapOptions)
 import Data.Function.Uncurried (Fn2, runFn2)
 
 foreign import data Map :: Type
 
-foreign import gMapImpl :: forall eff. Fn2 HTMLElement { zoom :: Number, center :: LatLng, mapTypeId :: String } (Eff eff Map)
+foreign import gMapImpl :: forall eff. Fn2 Element { zoom :: Number, center :: LatLng, mapTypeId :: String } (Eff eff Map)
 
-gMapFFI :: forall eff. HTMLElement -> { zoom :: Number, center :: LatLng, mapTypeId :: String } -> Eff eff Map
+gMapFFI :: forall eff. Element -> { zoom :: Number, center :: LatLng, mapTypeId :: String } -> Eff eff Map
 gMapFFI = runFn2 gMapImpl
 
-gMap :: forall eff. HTMLElement -> MapOptions -> Eff eff Map
+gMap :: forall eff. Element -> MapOptions -> Eff eff Map
 gMap e m = gMapFFI e (runMapOptions m)
 
 foreign import panToImpl :: forall eff. Fn2 Map LatLng (Eff eff Unit)
